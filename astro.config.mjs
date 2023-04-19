6ed87e11136a975e474490076d943abf3f8dd8aa
Ang Wei Feng (Ted) <ted.awf@outlook.com>
2023-04-19 15:47:41 +0800

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tedawf.com',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), sitemap()]
 });
