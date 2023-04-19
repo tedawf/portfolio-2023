f4a5ef1a2901d56464778bf66936a01426d17db9
Ang Wei Feng (Ted) <ted.awf@outlook.com>
2023-04-19 22:45:30 +0800

    fix rss to use content collections

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,27 +1,23 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 import filterProjects from "../utils/filterProjects";
-import type { CollectionEntry } from "astro:content";
+import { CollectionEntry, getCollection } from "astro:content";
 
-const postImportResult = await pagesGlobToRssItems(
-  import.meta.glob("./projects/**/*.md", { eager: true })
-);
-const projects: CollectionEntry<"projects">[] = filterProjects(
-  Object.values(postImportResult)
-);
+export async function get() {
+  const projects = await getCollection("projects");
+  const filteredProjects: CollectionEntry<"projects">[] =
+    filterProjects(projects);
 
-export const get = () =>
-  rss({
+  return rss({
     stylesheet: "/rss/styles.xsl",
     title: "Ted's Portfolio",
     description: "My personal portfolio created with the Astro framework",
     site: import.meta.env.SITE,
-    items: projects.map((project) => ({
-      link: project.slug,
+    items: filteredProjects.map((project) => ({
       title: project.data.title,
       pubDate: project.data.date,
       description: project.data.description,
-      customData: `
-    <author>${project.data.author}</author>
-    `,
+      customData: `<author>${project.data.author}</author>`,
+      link: `/projects/${project.slug}/`,
     })),
   });
+}
