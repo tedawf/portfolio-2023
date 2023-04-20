82255e6eb104dda7d7b6552fcabf6682c396bc4a
Ang Wei Feng (Ted) <ted.awf@outlook.com>
2023-04-20 11:28:40 +0800

    add tailwind to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tedawf.com/",
-  integrations: [
-    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
-    sitemap(),
-  ],
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), tailwind()]
 });
