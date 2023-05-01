fda5b5f93797c3195cf610a4045c0cf2d298d6a5
Ang Wei Feng (Ted) <ted.awf@outlook.com>
2023-05-02 01:11:41 +0800

    prettier plugin for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -13,6 +13,8 @@ export default defineConfig({
     }),
     sitemap(),
     tailwind({
+      // Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
       config: { applyBaseStyles: false },
     }),
   ],
